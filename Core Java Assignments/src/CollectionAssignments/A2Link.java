acf10fb1047d102c5b49d56847e4367c3f300854
rohrani <rohrani@LIN22004056.corp.capgemini.com>
2021-03-16 13:22:50 +0530

    second commit

--- Core Java Assignments/src/CollectionAssignments/A2Link.java
+++ Core Java Assignments/src/CollectionAssignments/A2Link.java
@@ -1,10 +1,20 @@
 package CollectionAssignments;
 
-public class A2Link {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
+import java.util.Iterator;  
+import java.util.LinkedHashSet;  
+  
+public class A2Link {   
+     public static void main(String args[]){    
+            LinkedHashSet<String> lh=new LinkedHashSet();    
+                   lh.add("A");     
+                   lh.add("B");      
+                   lh.add("C");     
+                   lh.add("D");    
+                   lh.add("E");    
+                   Iterator<String> i=lh.iterator();   
+                   while(i.hasNext())    
+                   {    
+                   System.out.println(i.next());    
+                   }    
+     }    
+    }  
